1a26756aff12ff0903c4f9861dbb2f01406ab18c
Mahek Bhatia <bhatiaindu28@gmail.com>
2026-05-31 21:35:29 +0530

    commit some chasnges

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="204214147803-dbru7s3cgd3qthlh1l2abm4nf7rbb612.apps.googleusercontent.com ">
+    <GoogleOAuthProvider clientId="204214147803-dbru7s3cgd3qthlh1l2abm4nf7rbb612.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>,
